8c7be8f6afcdeacd37c214354e6689930eb263d5
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2021-09-07 14:23:02 -0400

    Create AsssignmentB.sql

---
+++ AsssignmentB.sql
@@ -0,0 +1,5 @@
+-- Aaleeyah Ivy 9/7/2021 --
+-- Select these three fields from items and display them where unit price is more than 16 --
+SELECT unit_price, artist, title 
+FROM items 
+WHERE unit_price > 16 
