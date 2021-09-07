02a1cff92f9b83ad8f5d7302889b4c388ad003ef
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2021-09-07 14:51:18 -0400

    Add files via upload

---
+++ Assignment 1/AssignmentC.sql
@@ -0,0 +1,4 @@
+-- Aaleeyah Ivy 9/7/2021 --
+-- Selecting all customers by city & state --
+SELECT * FROM customers
+ORDER BY customer_state, customer_city
---
+++ Assignment 1/AssignmentD.sql
@@ -0,0 +1,5 @@
+-- Aaleeyah Ivy 9/7/2021--
+-- Selects all invoices with the vendor id 123 only --
+
+SELECT * FROM invoices
+WHERE vendor_id = 123
